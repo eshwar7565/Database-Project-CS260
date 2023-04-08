7f833265f405834d3ba8ead892c3934e67592800
eshwar7565 <eshwar_2101ai25@iitp.ac.in>
2023-04-09 04:39:09 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,74 @@
+
+<HTML>
+<HEAD>
+<TITLE> TPC  </TITLE>
+<STYLE  TYPE="text/css">
+
+A {font-family: Verdana, Arial, Helvetica, sans-serif, "MS sans serif";font-size: 14px;line-height: 13px;text-decoration: none;color: #ddeeff;font-weight: bold;}
+A:hover
+{color:#6666CC;text-decoration:none;}
+h1 {font-family: Georgia,Verdana, Arial, Helvetica, sans-serif, "MS sans serif";font-size: 24px;line-height: 140%;text-decoration: none;color: #000000;font-weight: bold;}
+h2 {font-family: Georgia,Verdana, Arial, Helvetica, sans-serif, "MS sans serif";font-size: 18px;line-height: 130%;text-decoration: none;color: #000000;font-weight: bold;}
+.dark {font-family: Verdana, Arial, Helvetica, sans-serif, "MS sans serif";font-size: 11px;line-height: 13px;text-decoration: none;color: #000099;font-weight: bold;}
+.para {font-family: Verdana, Arial, Helvetica, sans-serif, "MS sans serif";font-size: 11px;line-height: 13px;text-decoration: none;color: #787878;font-weight: bold;}
+.big {font-family: Georgia,Verdana, Arial, Helvetica, sans-serif, "MS sans serif";font-size: 20px;line-height: 100%;text-decoration: none;color: Crimson�;font-weight: bold;}
+
+</STYLE>
+
+<script type='text/javascript' src='script/var.js'></script>
+<script type='text/javascript' src='script/status.js'></script>
+
+</HEAD>
+<BODY>
+
+
+<table height = "100%" width="100%" cellspacing="0" cellpadding="0">
+<tr height = "15%"><td>
+<table  width="100%" cellspacing="0" cellpadding="0">
+	<tr><td> <tr> <td align="center"><h1><font color = DarkBlue>Indian Institute of Technology Patna</h1></td> </tr>
+
+ </td> </tr>
+</table>
+</td></tr>
+<tr height = "50%"><td>
+<table  width="100%" align="center" cellspacing="0" cellpadding="0"  border="0">
+	<tr> <td align="center"> <img src="pics/iitpatna_admin.jpg" > </td> </tr>
+</table>
+</td></tr>
+<tr height = "30%"><td>
+<table  align = "center" border=0 cellspacing=0 cellpadding=4>
+<tr><td width=30>&nbsp;</td>
+<td bgcolor=skyblue� align=center width=100><a href="/student/login2.asp" alt ="Id" title = "For Student">Student </a></td>
+<td width=30>&nbsp;</td>
+<td width=30>&nbsp;</td>
+<td bgcolor=red align=center width=100 ><a href="/student/login_admin1.asp" alt ="Id" title = "For Administration">Alumni</a></td>
+<td width=30>&nbsp;</td>
+<td width=30>&nbsp;</td>
+<td bgcolor=indigo align=center width=100 ><a href="/student/fac_login_new.asp" alt ="Id" title = "For Faculty">Company</a></td>
+<td width=30>&nbsp;</td>
+
+<td width=30>&nbsp;</td>
+</tr>
+<tr><td colspan=12 bgcolor=#3366cc><img width=1 height=1 alt=""></td></tr>
+<tr><td colspan=15> &nbsp;</td></tr>
+</table>
+</td></tr>
+<tr height = "7%"><td>
+<table   width="100%" cellspacing="0" cellpadding="0">
+	<tr><td> <TABLE WIDTH="100%" cellspacing="0" cellpadding="0">
+<TR>
+<TD bgcolor=lightgrey class="dark" align="center" height="60" valign="center">
+
+<b>Placement Portal<br>Indian Institute of Technology Patna  <BR>
+  <br></b>
+
+</TD>
+</TR>
+</TABLE>
+ </td> </tr>
+</table>
+</td></tr>
+</table>
+
+</BODY>
+</HTML>

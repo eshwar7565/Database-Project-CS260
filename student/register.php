98b15966af27e7ec5d264f103606925224c10180
eshwar7565 <eshwar_2101ai25@iitp.ac.in>
2023-04-09 15:44:59 +0530

    working in student section

--- student/register.php
+++ student/register.php
@@ -4,33 +4,40 @@ include 'config.php';
 if (isset($_POST['submit'])) {
 	//Save all values given in respective variables 
 	
-	$name1 = $_POST['name1'];
-	$name2 = $_POST['name2'];
-	
-	$email = $_POST['email'];
+	$rollno = $_POST['rollno'];$email = $_POST['email'];$c10 = $_POST['c10'];
+	$c12 = $_POST['c12'];$cpi = $_POST['cpi'];$special = $_POST['special'];
+	$aoi = $_POST['aoi'];$dept = $_POST['dept'];$age = $_POST['age'];
+
+
+// 	$filename = $_FILES['resume']['name'];
+// $tempFilePath = $_FILES['resume']['tmp_name'];
+
+// // Read the file contents
+// $resumeData = addslashes(file_get_contents($tempFilePath));
 	
 	$pwd1 = $_POST['pwd1'];
 	$pwd2 = $_POST['pwd2'];
+	
 
 	//If password does not match confirm password then throw error 
 	if ($pwd1 != $pwd2) {
 		echo "<script>alert('Passwords do not match.')</script>";
 	} else {
 		//Check if user with the same employee id already exists
-		$query = "SELECT * FROM users where email='$email' ";
+		$query = "SELECT * FROM sd where rollno='$rollno' ";
 		$result = mysqli_query($conn, $query);
 		if (mysqli_num_rows($result) > 0) {
 			echo "<script>alert('User already registered. Please login.')</script>";
 		} else {
 			//Insert new employee entry into database 
-			$query = "INSERT INTO users ( first_name, last_name, email,pass) 
-				VALUES ('$name1','$name2','$email','$pwd1')";
+			$query = "INSERT INTO sd ( rollno,webmail,c10,c12,cpi,special,aoi,branch,age,pass) 
+				VALUES ('$rollno','$email',$c10,$c12,$cpi,'$special','$aoi','$dept','$age','$pwd1')";
 
 			$result = mysqli_query($conn, $query);
 			//If insertion is successful, then redirect to login page else throw error 
 			if ($result) {
 				echo "<script>alert('User registerd!')</script>";
-				header("Location: index.php");
+				header("Location: login.php");
 			} else {
 				echo "<script>alert('Something went wrong!')</script>";
 			}
@@ -73,25 +80,52 @@ if (isset($_POST['submit'])) {
 
 							</div>
 						</div>
+
 						<div class="form-group">
-							<label for="name1" class="form-label">First Name</label>
-							<input type="text" class="form-control" id="name1" name="name1" placeholder="First Name" required />
+							<label for="rollno" class="form-label">Roll No</label>
+							<input type="text" class="form-control" id="rollno" name="rollno" placeholder="Roll No" required />
 						</div>
-						<div class="form-group">
-							<label for="name2" class="form-label">Last Name</label>
-							<input type="text" class="form-control" id="name2" name="name2" placeholder="Last Name" required />
-						</div>
-						
-							
+
 						<div class="row">
 							<div class="col-md-6 mb-4">
 								<div class="form-group">
-									<label class="form-label" for="email">Email ID</label>
+									<label class="form-label" for="email">Webmail </label>
 									<input type="email" class="form-control" name="email" id="email" placeholder="Email" required />
 								</div>
 							</div>
 							
 						</div>
+						<div class="form-group">
+							<label for="c10" class="form-label">Class X Marks</label>
+							<input type="text" class="form-control" id="c10" name="c10" placeholder="Class X Marks" required />
+						</div>
+						<div class="form-group">
+							<label for="c12" class="form-label">Class XII Marks</label>
+							<input type="text" class="form-control" id="c12" name="c12" placeholder="Class XII Marks" required />
+						</div>
+						<div class="form-group">
+							<label for="cpi" class="form-label">CPI</label>
+							<input type="text" class="form-control" id="cpi" name="cpi" placeholder="CPI" required />
+						</div>
+						<div class="form-group">
+							<label for="Specialization" class="form-label">Specialization</label>
+							<input type="text" class="form-control" id="special" name="special" placeholder="Specialization" required />
+						</div>
+						
+						<div class="form-group">
+							<label for="Area of Interest" class="form-label">Area of Interest</label>
+							<input type="text" class="form-control" id="aoi" name="aoi" placeholder="Area of Interest" required />
+						</div>
+						
+						<div class="form-group">
+							<label for="Department" class="form-label">Department</label>
+							<input type="text" class="form-control" id="dept" name="dept" placeholder="Department" required />
+						</div>
+						<div class="form-group">
+							<label for="age" class="form-label">Age</label>
+							<input type="text" class="form-control" id="age" name="age" placeholder="Age" required />
+						</div>
+
 						<div class="form-group">
 							<label for="pwd1" class="form-label">Password</label>
 							<input type="password" class="form-control" name="pwd1" id="pwd1" placeholder="Password" required />
@@ -100,11 +134,17 @@ if (isset($_POST['submit'])) {
 							<label for="pwd2" class="form-label">Confirm Password</label>
 							<input type="password" class="form-control" name="pwd2" id="pwd2" placeholder="Confirm Password" required />
 						</div>
+						
+                        <!-- <label for="resume">Upload your resume</label>
+                       <br>
+                      <input type="file" name="resume" id="resume">
+					
+                       <br> -->
 						<div class="row">
 							<div style="align-items: center;padding-left: 3%;">
 								<button type="submit" name="submit"  class="btn btn-default">Register</button>
 								
-								      <br><br>Already a user? <a href="index.php">Login</a>
+								      <br><br>Already a user? <a href="login.php">Login</a>
 							</div>
 						</div>
 					</form>

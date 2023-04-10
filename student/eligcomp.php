3d8143300b64b6b369d9d05ed2238a7b359b88e5
eshwar7565 <eshwar_2101ai25@iitp.ac.in>
2023-04-10 10:33:38 +0530

    table

--- student/eligcomp.php
+++ student/eligcomp.php
@@ -14,3 +14,16 @@ require 'config.php';
     }
       
 ?>
+<?php
+$sql = "SELECT compname FROM companyregister";
+$result = mysqli_query($conn, $sql);
+
+// Display the list of companies on the web page
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<h2>" . $row["compname"] . "</h2>";
+   
+    echo "<a href='apply.php?id=" . $row["compname"] . "'>Apply</a>";
+}
+
+mysqli_close($conn);
+?>

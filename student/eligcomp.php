317cd429cdf3047556094cc56b736d0d16373bf5
eshwar7565 <eshwar_2101ai25@iitp.ac.in>
2023-04-15 22:31:18 +0530

    export

--- student/eligcomp.php
+++ student/eligcomp.php
@@ -52,7 +52,9 @@ echo "<table style='border-collapse: collapse; margin: 20px; border: 2px solid #
 echo "<tr><th style='border: 2px solid #333; padding: 10px; color: #fff; background-color: #333;'>Company Name</th>
 <th style='border: 2px solid #333; padding: 10px; color: #fff; background-color: #333;'>Type</th>
 <th style='border: 2px solid #333; padding: 10px; color: #fff; background-color: #333;'>Mode</th>
-<th style='border: 2px solid #333; padding: 10px; color: #fff; background-color: #333;'>Update</th></tr>";
+<th style='border: 2px solid #333; padding: 10px; color: #fff; background-color: #333;'>Salary</th>
+<th style='border: 2px solid #333; padding: 10px; color: #fff; background-color: #333;'>Update</th>
+</tr>";
 
 while ($row = mysqli_fetch_assoc($result)) {
   echo "<tr>";
@@ -60,6 +62,7 @@ while ($row = mysqli_fetch_assoc($result)) {
   echo "<td style='border: 2px solid #333; padding: 10px;'>" . $row['compname'] . "</td>";
   echo "<td style='border: 2px solid #333; padding: 10px;'>" . $row['type'] . "</td>";
   echo "<td style='border: 2px solid #333; padding: 10px;'>" . $row['mode'] . "</td>";
+  echo "<td style='border: 2px solid #333; padding: 10px;'>" . $row['salary'] . "</td>";
   echo "<td style='border: 2px solid #333;style='padding: 10px;'><a href='apply.php?id=" . $row["compname"] . "' style='padding: 5px 10px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 3px;'>Apply Now</a></td>";
   echo "</tr>";
 }
